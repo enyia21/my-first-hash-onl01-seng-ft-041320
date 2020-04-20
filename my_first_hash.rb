1cfaf9521834f57f3e62ce0804e12f03f42f322d
Chibuisi Enyia <enyia21@gmail.com>
2020-04-20 16:59:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {"Coding School" => "Flatiron", "Favorite Team" => "Chicago Bears", "College" => "University of Illinois"}
+  
   
 end
 
